e8cbd03e441480f0814a0ee312f25cde06e0a8a6
0akkung <pawat.put@ku.th>
2023-10-25 23:03:40 +0700

    Modify home page

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/customer', function () {
     return view('customer.index');
 });
