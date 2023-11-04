4bd521510c1a32862dad871baee18c6fa191dcce
Petchgamon Sangnual <petchgamon.s@ku.th>
2023-11-04 16:29:38 +0700

    update ui

--- resources/views/deliveries/create.blade.php
+++ resources/views/deliveries/create.blade.php
@@ -9,11 +9,12 @@
     <form action="{{ route('deliveries.store') }}" method="POST">
         @csrf
         <div class="mb-6">
-            <label for="saleOrder_id" class="block font-bold mb-4">Select Sale Order</label>
+            <label for="saleOrder_id" class="block font-bold mb-4 text-xl">Select Sale Order</label>
             <select id="saleOrder_id" name="saleOrder_id" class="border rounded-lg px-20 mb-4">
                 @foreach($saleOrders as $saleOrder)
                     <option value="{{ $saleOrder->id }}" data-customer="{{ $saleOrder->purchaseOrder->customer }}" 
-                        data-purchaseorder="{{$saleOrder->purchaseOrder}}"> {{ $saleOrder->id }}</option>    
+                        data-purchaseOrder="{{$saleOrder->purchaseOrder}}"> 
+                        {{ $saleOrder->id }}</option>    
                 @endforeach
             </select>
             <div id="soShow"></div>
@@ -43,20 +44,34 @@
     window.onload = function() {
     var selectElement = document.getElementById("saleOrder_id");
     var selectedValueElement = document.getElementById("soShow");
-    var tableElement = document.getElementById("soItemsTable");
-    var selectElement = document.getElementById("saleOrder_id");
-    var selectedValueElement = document.getElementById("soShow");
-    
+
     selectElement.addEventListener("change", function() {
         var selectedValue = selectElement.value;
         var selectedOption = selectElement.options[selectElement.selectedIndex];
         var customer = JSON.parse(selectedOption.getAttribute("data-customer"));
-        var purchaseOrder = JSON.parse(selectedOption.getAttribute("data-purchaseorder"));
+        var purchaseOrder = JSON.parse(selectedOption.getAttribute("data-purchaseOrder"));
+    
         var htmlContent = `
-        <p>SO ID: ${selectedValue}</p>
-        <p>PO ID: ${purchaseOrder.id}</p>
-        <p>Customer ID: ${customer.id}</p>
-        <p>Company Name: ${customer.company_name}</p>
+        <div class="flex">
+            <div class="text-lg">
+                <span class="text-gray-600 font-semibold">SO ID : </span> 
+                <span class="font-bold ml-3">${selectedValue}<span>
+            </div>
+            <div class="text-lg ml-48">
+                <span class="text-gray-600 font-semibold">PO ID : </span> 
+                <span class="font-bold ml-3">${purchaseOrder.id}<span>
+            </div>
+        </div>
+        <div class="flex mb-5">
+            <div class="text-lg">
+                <span class="text-gray-600 font-semibold">Customer ID : </span> 
+                <span class="font-bold ml-3"> ${customer.id}<span>
+            </div>
+            <div class="text-lg ml-36">
+                <span class="text-gray-600 font-semibold">Company Name : </span> 
+                <span class="font-bold ml-3">${customer.company_name}<span>
+            </div>
+        </div>
     `;
     selectedValueElement.innerHTML = htmlContent;
     });
